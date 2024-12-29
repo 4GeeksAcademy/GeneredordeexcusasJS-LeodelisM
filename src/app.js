92159725a4dfd227041fd87910c161025b4a9447
Leodelis <leodelis19@gmail.com>
2024-12-29 18:06:07 +0000

    GeneradorExcu1.0

--- src/app.js
+++ src/app.js
@@ -2,10 +2,30 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function seleccionarElementoAleatorio(arr) {
+  const idx = Math.floor(Math.random() * arr.length);
+  return arr[idx];
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const randomWho = seleccionarElementoAleatorio(who);
+  const randomAction = seleccionarElementoAleatorio(action);
+  const randomWhat = seleccionarElementoAleatorio(what);
+  const randomWhen = seleccionarElementoAleatorio(when);
+
+  const randomExcuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+
+  const excuseElement = document.querySelector("#excuse");
+  excuseElement.innerHTML = randomExcuse;
 };
